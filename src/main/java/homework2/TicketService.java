aa70f313eb655cb39063c3859265b1fefe6d82c2
MaVaYuran <mariayuran@yandex.by>
2024-09-02 18:39:39 +0300

    hw3 ticket store remove id

--- src/main/java/homework2/TicketService.java
+++ src/main/java/homework2/TicketService.java
@@ -1,8 +1,7 @@
 package homework2;
 
 public class TicketService {
-    private static final String ID = "1";
-    public static void main(String[] args) {
+      public static void main(String[] args) {
 
         for (int i = 0; i < 10 ; i++) {
             Ticket ticket = new Ticket("LaScala", i);
