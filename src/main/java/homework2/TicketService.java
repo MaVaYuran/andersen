877fea002773c9049df88528c1e5ab035eceb3a9
MaVaYuran <mariayuran@yandex.by>
2024-09-02 19:39:29 +0300

    hw3 function returning method by id

--- src/main/java/homework2/TicketService.java
+++ src/main/java/homework2/TicketService.java
@@ -1,12 +1,35 @@
 package homework2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TicketService {
-      public static void main(String[] args) {
+    static List<Ticket> tickets = new ArrayList<>();
 
-        for (int i = 0; i < 10 ; i++) {
-            Ticket ticket = new Ticket("LaScala", i);
-            System.out.println("ticket Number: " + (i+1) + " - " + ticket);
+    public static void main(String[] args) {
 
+        int count = 1;
+
+        for (int i = 0; i < 10; i++) {
+            String id = Integer.toString(count);
+            Ticket ticket = new Ticket(id, "LaScala", (i+100), true, Sector.B, 10.0f, 9.99);
+            count++;
+            tickets.add(ticket);
         }
+
+        System.out.println(tickets);
+
+        System.out.println("Ticket from method: " + getTicketById("8"));
     }
+
+    static Ticket getTicketById(String id) {
+        for (Ticket ticket : tickets) {
+            if (id.equals(ticket.getId())) {
+                return ticket;
+            }
+        }
+        return null;
+    }
+
+
 }
