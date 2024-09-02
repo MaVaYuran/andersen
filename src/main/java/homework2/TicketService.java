532856dff3ff012f69cb2091276123de26d30ce0
MaVaYuran <mariayuran@yandex.by>
2024-09-02 14:59:28 +0300

    hw3 ticket store

--- src/main/java/homework2/TicketService.java
+++ src/main/java/homework2/TicketService.java
@@ -4,17 +4,10 @@ public class TicketService {
     private static final String ID = "1";
     public static void main(String[] args) {
 
-        Ticket fullTicket = new Ticket(ID,"Plaza", 290, false, Sector.A, 25.00f, 15.99);
-        Ticket limitedTicket = new Ticket("LaScala", 180);
-        Ticket emptyTicket = new Ticket();
+        for (int i = 0; i < 10 ; i++) {
+            Ticket ticket = new Ticket("LaScala", i);
+            System.out.println("ticket Number: " + (i+1) + " - " + ticket);
 
-
-
-        System.out.println("Full ticket:");
-        System.out.println(fullTicket);
-        System.out.println("Limited ticket:");
-        System.out.println(limitedTicket);
-        System.out.println("Empty ticket:") ;
-        System.out.println(emptyTicket);
+        }
     }
 }
